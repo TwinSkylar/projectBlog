68506a1019ce319ec0b8fe6061b195c1392f8ee7
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-07-02 10:28:57 -0400

    Moved the projects routes into their own file

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import "dotenv/config";
 import mongoose from "mongoose";
-import Project from "./models/project.mjs";
+import projectRoutes from './routes/projectRoutes.mjs'
 
 const app = express();
 
@@ -34,85 +34,8 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-// project routes
-app.get("/projects", (req, res) => {
-  Project.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Projects", projects: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/projects", (req, res) => {
-  const tags = ["test1, test2, test3"];
-  const project = new Project(req.body);
-  project.tag = tags;
-  project
-    .save()
-    .then((result) => {
-      res.redirect("projects");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/projects/:id", (req, res) => {
-  const id = req.params.id;
-  Project.findById(id)
-    .then((result) => {
-      res.render("details", { project: result, title: "Project Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/projects/:id", (req, res) => {
-  const id = req.params.id;
-  Project.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({redirect: '/projects'})
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/add-project", (req, res) => {
-  const project = new Project({
-    title: "Pokemon Store 2",
-    codeUrl: "https://github.com/TwinSkylar/shoppingcart",
-    pageUrl: "https://twinskylar.github.io/shoppingcart/",
-    tag: ["javascript", "html", "react", "css"],
-    description: "A pokemon store which lets you fill up a shopping cart",
-  });
-  project
-    .save()
-    .then((result) => {
-      res.redirect("projects");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/all-projects", (req, res) => {
-  Project.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/projects/create", (req, res) => {
-  res.render("create", { title: "Create a new project" });
-});
+//project routes
+app.use('/projects',projectRoutes);
 
 //404 page
 app.use((req, res) => {
